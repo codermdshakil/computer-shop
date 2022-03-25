7da45da501ef127696caf7c8a91107992a756029
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-03-26 04:10:13 +0600

    some change that i make

--- src/component/AllProducts/AllProducts.js
+++ src/component/AllProducts/AllProducts.js
@@ -18,17 +18,26 @@ const AllProducts = () => {
     },[]);
 
     const addToCart = (product) => {
-        document.getElementById('warning').style.display="none";
-        document.getElementById('random-container').style.display="block";
         const newCart = [...cart, product];
         setCart(newCart);
+        // warning massage 
+        document.getElementById('warning').style.display="none";
+        // selected product container 
+        document.getElementById('selected-item').style.display="block";
+        // random product container 
+        document.getElementById('random-container').style.display="none";
+
+
     }
 
-    const remoteItem = (product) => {
-        document.getElementById('warning').style.display="block";
-        document.getElementById('random-container').style.display="none";
+    const removeItem = (product) => {
         const remoteItem = [];
         setCart(remoteItem);
+
+        // warning massage 
+        document.getElementById('warning').style.display="block";
+        // random product container 
+        document.getElementById('random-container').style.display="none";
     }
 
 
@@ -45,16 +54,15 @@ const AllProducts = () => {
 
 
     const randomProduct = (cart) => {
-        document.getElementById('selected-item').style.display='none';
-        document.getElementById('warning').style.display="none";
-        document.getElementById('random-container').style.display="block";
-        document.getElementById('win').style.display="block";
-    
         const randomNumber = Math.random() * cart.length-1;
         const randomValue = Math.floor(randomNumber);
         let index = getRandomProduct(0, randomValue);
         cart = cart[index];
         setRandomItem(cart);
+        document.getElementById('selected-item').style.display="none";
+        document.getElementById('win').style.display="block";
+        // random product container 
+        document.getElementById('random-container').style.display="block";
     }
    
     return (
@@ -70,22 +78,22 @@ const AllProducts = () => {
                     </div>
                 </div>
                 <div className="col-lg-3 col-md-4 col-10 d-block m-auto m-md-0 order-first order-md-last">
-                    <div className='show-products'>
+                    <div className='show-products border'>
                         <h4 className='selectes-title'>Selected Products</h4>
                         <div>
-                            <p id='warning'>Please Select 4 Product!!</p>
+                            <p id='warning'>Please Select Product!!</p>
                             <div id='selected-item'>
                                 {
                                     cart.map(item => <SelectedProducts key={item.id} item={item}></SelectedProducts>)
                                 }
                             </div>
                             <div id='random-container' >
-                                <p id='win'>this is for you!!😊</p>
+                                <p id='win'>this is for you!! 😊</p>
                                 <RandomProduct key={randomItem.id} product={randomItem}></RandomProduct>
                             </div>
                         </div>
                         <button className='chooseOne-btn' onClick={()=>randomProduct(cart)}>Choose 1 for me</button><br/>
-                        <button className='chooseAgain-btn' onClick={() => remoteItem(products)}>Choose Again</button>
+                        <button className='chooseAgain-btn' onClick={() => removeItem(products)}>Choose Again</button>
                     </div>
                 </div>
             </div>
