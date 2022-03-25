144baf9c0acc1cbefcbab1b88878aef3579986fc
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-03-25 23:13:02 +0600

    button icon added

--- src/component/AllProducts/AllProducts.js
+++ src/component/AllProducts/AllProducts.js
@@ -15,17 +15,16 @@ const AllProducts = () => {
     return (
         <div>
             <h2 className='main-title' style={{textAlign:'center'}}>Welcome to <span className='title'>Computer Shop</span></h2>
-            <h3 className='semi-title'>Products Overview</h3>
-
             <div className="row">
-                <div className="col-lg-8 col-md-8 col-10">
+                <div className="col-lg-9 col-md-8 col-12">
                     <div className="row">
+                    <h3 className='semi-title text-center text-md-start'>Products Overview</h3>
                         {
                             products.map(product => <Product product={product} key={product.id}></Product>)
                         }
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-4 col-10 order-first order-md-last">
+                <div className="col-lg-3 col-md-4 col-10 order-first order-md-last">
                     <h2>Selected</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, nesciunt id vel in sunt natus accusamus! Beatae, asperiores quod illum mollitia harum porro excepturi ea dolorum. Cumque, atque sit. Quisquam tempore nihil dolor enim eveniet, dolorem incidunt corporis debitis veritatis illo sequi. Nobis voluptas dolorum sunt earum repudiandae hic commodi incidunt labore, ullam officia accusantium, voluptatem iure? Aliquam obcaecati veritatis nemo officiis deleniti laborum quos doloremque fugit tempore corrupti ullam unde nihil a eius modi minus doloribus provident, autem ex repellat voluptates. Corporis quae, perferendis molestiae hic tenetur exercitationem placeat debitis? Adipisci atque cum mollitia repudiandae deleniti illum eligendi ex.</p>
                 </div>
--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const Product = ({product}) => {
 
@@ -6,12 +9,18 @@ const Product = ({product}) => {
     console.log(img, name, price); 
 
     return (
-        <div>
-            <div className='product-box'>
-                
+        <div className='col-lg-4 col-md-6 col-10 d-block m-auto m-md-0'>
+            <div className='single-product border'>
+                <img src={img} alt="" />
+                <h5>{name}</h5>
+                <p>Price: ${price}</p>
+                <div>
+                    <button>Add To Cart <FontAwesomeIcon icon={faCartShopping}/></button>
+                </div>
             </div>
         </div>
     );
 };
 
 export default Product;
+
